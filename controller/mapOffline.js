8e70b13653e622c8cd54a96e15ee6be7171b6f0d
Yury Rivera <yrivera@applaudostudios.com>
2021-08-17 11:05:46 -0600

    updating stuff

--- controller/mapOffline.js
+++ controller/mapOffline.js
@@ -1,160 +1,116 @@
-    const bufferSize = 16 * 1024
-    const filesDir = __dirname + '\\'
-    var total = 0
-    var listPoints = new Array
-    const mLimit = 12
-    const nLimit = 19
-    const round = 0.05
-    var progress = 0
-    var data = null
-    var fallas = 0
-    var exception = null
+const imagenDownload = require('./imagenDownload').download
+const fs = require('fs');
+const filesDir = __dirname + '\\'
+let total = 0
+var listPoints = new Array
+const mLimit = 19
+const nLimit = 19
+const round = 0.05
+let progress = 0
+let data = null
+let fallas = 0
 
-    class Point {
-        constructor(x, y) {
-            this.x = x;
-            this.y = y;
-        }
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
+}
 
-    async function doWork() {
-        //console.log("in doWork init");
-        defineFullMap()
-        console.log("mapa definido")
-        progress = 0
-        var flagList = 0
-        for (k=12; 12<=19; k++) {
-            for (i=listPoints[flagList].x; i <= listPoints[flagList + 1].x; i++) {
-                var divide = listPoints[flagList + 1].y - listPoints[flagList].y
-                divide /= 20
-                divide = Math.trunc(divide)
-                if (divide == 0){
-                    divide = 1
-                }
-                var initialvalue =  listPoints[flagList].y
-                var corrutines = new Array
-                //await downloadPart(i, k, initialvalue, initialvalue + divide)
-                while (initialvalue < listPoints[flagList + 1].y){
-                    corrutines.push(downloadPart(i, k, initialvalue, initialvalue + divide))
-                        //progress += await 
-                    //console.log("progreso", progress);
-                    initialvalue += divide
-                    if (exception != null){
-                        console.log("descarga mapa","fallo por inter")
-                        return Result.failure()
-                    }
-                }
-                await corrutines[0]
-                if (exception != null){
-                    console.log("descarga mapa","fallo por inter")
-                    return Result.failure()
-                }
+async function doWork() {
+    defineFullMap()
+    var flagList = 0
+    for (k = nLimit; k <= mLimit; k++) {
+        for (i = listPoints[flagList].x; i <= listPoints[flagList + 1].x; i++) {
+            var divide = listPoints[flagList + 1].y - listPoints[flagList].y
+            divide /= 10
+            divide = Math.trunc(divide)
+            if (divide == 0) {
+                divide = 1
             }
-            flagList += 2
-            if (exception != null){
-                return Result.failure()
-            }
-        }
-
-        return Result.success()
-    }
-
-    async function downloadPart(i, k, a, b) {
-        var flag = 0;
-        for (j=a; a<=b; a++) {
-            let exceptione = await downloadTileImage(i, j, k);
-            //await sleep(1000)
-            if (exceptione != null) {
-                exception = exceptione
-                console.log("descarga mapa","fallo por inter")
-                break
-            }
-            flag++
-        }
-        return flag
-    }
-
-    function defineFullMap(){
-        var latTemp = null
-        var longTemp = null
-        var flag = 0
-        for (i=mLimit; i <= nLimit; i++) {
-            latTemp = 14.4179
-            longTemp = -90.1397
-            listPoints.push(new Point(getXm(longTemp, i), getYm(latTemp, i)))
-            latTemp = 13.14
-            longTemp = -87.7112
-            listPoints.push(new Point(getXm(longTemp, i), getYm(latTemp, i)))
-            //console.log("listpoint",listPoints);
-            total +=
-                (listPoints[flag].x - listPoints[flag + 1].x) * (listPoints[flag].y - listPoints[flag + 1].y)
-            flag += 2
-        }
-    }
-
-    function getXm(longitude, i) {
-        return (Math.floor((longitude + 180) / 360 * (2**i)))
-    }
-
-    function getYm(latitude, i) {
-        return Math.floor(
-            (1 - Math.log(
-                ((latitude)*Math.PI/180)
-                        + (1 / Math.cos((latitude*Math.PI/180)))
-            ) / Math.PI
-                    ) * (2**(i - 1))
-        )
-    }
-
-    const imagenDownload = require('./imagenDownload').download
-
-    const fs = require('fs');
-
-    async function downloadTileImage(x, y, zoom) {
-        //console.log("donwload tile", `progreso: ${progress}, fallas: ${fallas}, total: ${total}`)
-        let name = getTileFileDir(x, zoom) + `${y}.png`
-        console.log("nombre de archivo", name);
-        try {
-            //console.log("prede-verifique file")
-            try {
-                if (fs.existsSync(name)) {
-                    console.log(`${getTileFileDir2(x, zoom) + `${y}.png`} Tile Exist!!`);
-                    return null
-                } else{
-                    let urls =`https://a.tile.openstreetmap.org/${zoom}/${x}/${y}.png`;
-                    //console.log("pre download")
-                    await imagenDownload(urls, getTileFileDir(x, zoom), `${y}.png`, ()=>{
-                        console.log(`${urls} Tile download!!`);
-                    })
-                //await sleep(1000)
+            var initialvalue = listPoints[flagList].y
+            while (initialvalue < listPoints[flagList + 1].y) {
+                await downloadPart(i, k, initialvalue, initialvalue + divide);
+                progress ++
+                if (progress % 200 === 0) {
+                    // for it to print every 200 images instead of 1 every image
+                    console.log(`progreso: ${progress}, fallas: ${fallas}, total: ${total}`)
                 }
-            } catch (error) {
-                let urls =`https://a.tile.openstreetmap.org/${zoom}/${x}/${y}.png`;
-                console.log("pre download", error)
-                /*await imagenDownload(urls, getTileFileDir(x, zoom), `${y}.png`, ()=>{
-                    console.log(`${urls} Tile download!!`);
-                })*/
-                //sleep(1000)
-            }
-        } catch (e) {
-            //console.log("tileP", "outmemory", e)
-            return e
+                initialvalue += divide
+            } 
         }
-        return null
+        flagList += 2
     }
+    console.log("FINISH");
+}
 
-    function getTileFileDir(x, zoom) {
-        return `${filesDir}map\\${zoom}\\${x}\\`
+async function downloadPart(i, k, a, b) {
+    var flag = 0;
+    for (j = a; a <= b; a++) {
+        let exceptione = await downloadTileImage(i, j, k);
+        if (exceptione != null) {
+            fallas++;
+            break
+        }
+        flag++
     }
-    function getTileFileDir2(x, zoom) {
-        return `/map/${zoom}/${x}/`
+}
+
+function defineFullMap() {
+    var latTemp = null
+    var longTemp = null
+    var flag = 0
+    for (i = mLimit; i <= nLimit; i++) {
+        latTemp = 14.4179
+        longTemp = -90.1397
+        listPoints.push(new Point(getXm(longTemp, i), getYm(latTemp, i)))
+        latTemp = 13.14
+        longTemp = -87.7112
+        listPoints.push(new Point(getXm(longTemp, i), getYm(latTemp, i)))
+        total +=
+            (listPoints[flag].x - listPoints[flag + 1].x) * (listPoints[flag].y - listPoints[flag + 1].y)
+        flag += 2
     }
+    console.log("mapa definido")
+}
 
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+function getXm(longitude, i) {
+    return (Math.floor((longitude + 180) / 360 * (2 ** i)))
+}
+
+function getYm(latitude, i) {
+    return Math.floor(
+        (1 - Math.log(
+            ((latitude) * Math.PI / 180)
+            + (1 / Math.cos((latitude * Math.PI / 180)))
+        ) / Math.PI
+        ) * (2 ** (i - 1))
+    )
+}
+
+async function downloadTileImage(x, y, zoom) {
+    let name = getTileFileDir(x, zoom) + `${y}.png`
+    try {
+        if (fs.existsSync(name) && fs.e && fs.readFileSync.length > 0) {
+            console.log(`Cache hit: ${getTileFileDir2(x, zoom) + `${y}.png`}`);
+        } else {
+            let urls = `https://a.tile.openstreetmap.org/${zoom}/${x}/${y}.png`;
+            await imagenDownload(urls, getTileFileDir(x, zoom), `${y}.png`, () => {
+                console.log(`Tile download: ${urls}`);
+            })
+        }
+        return null;
+    } catch (e) {
+        return e
     }
+}
 
+function getTileFileDir(x, zoom) {
+    return `${filesDir}map\\${zoom}\\${x}\\`
+}
 
-    module.exports = {
-        doWork
-    };
+function getTileFileDir2(x, zoom) {
+    return `/map/${zoom}/${x}/`
+}
+
+module.exports = { doWork };
