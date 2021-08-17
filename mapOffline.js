2c753530d90816456e194d785efc168e74577b2d
Yury Rivera <yrivera@applaudostudios.com>
2021-08-17 14:26:01 -0600

    removiendo express

--- mapOffline.js
+++ mapOffline.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const filesDir = __dirname + '\\'
 let total = 0
 var descargando = 0;
-var existencia = 0
-var listPoints = new Array
+var existencia = 0;
+var listPoints = [];
 const mLimit = 15
 const nLimit = 15
 let progress = 0
@@ -22,44 +22,29 @@ async function doWork() {
     console.log("to preocess");
     console.log("poinst", listPoints)
     console.log("total", total)
-    processMap()
-    
+    await processMap()
 }
 
 async function processMap(){
     var flagList = 0
-    //var flagForOne = 0
-    //var flagForTwo = 0
     for (k = nLimit; k <= mLimit; k++) {
         for (i = listPoints[flagList].x; i <= listPoints[flagList + 1].x; i++) {
-            //flagForOne++;
             var divide = listPoints[flagList + 1].y - listPoints[flagList].y
             divide /= 10
             divide = Math.trunc(divide)
-            if (divide == 0) {
+            if (divide === 0) {
                 divide = 1
             }
             var initialvalue = listPoints[flagList].y
-            //var threads = new Array
             while (initialvalue < listPoints[flagList + 1].y) {
                 await downloadPart(i, k, initialvalue, initialvalue + divide);
-                //flagForTwo += divide;
-                //await downloadPart(i, k, initialvalue, initialvalue + divide);
-                //progress ++
+                progress ++
                 initialvalue += divide
-            } 
-            /*if (threads.length > 3){
-                await threads[threads.length - 2]
-            } else {
-                await threads[0]
-            }*/
-            
+            }
+
         }
         flagList += 2
     }
-    console.log("FINISH");
-    //console.log("for one", flagForOne)
-    //console.log("for two", flagForTwo)
 }
 
 async function downloadPart(i, k, a, b) {
@@ -69,7 +54,7 @@ async function downloadPart(i, k, a, b) {
         let exceptione = await downloadTileImage(i, j, k);
         if (exceptione != null) {
             fallas++;
-            break
+            break;
         }
         flag++
         console.log(`de ${a} a ${b}.  ${flag}/${totalint}. fallas ${fallas}. progreso ${progress}/${total}, existen: ${existencia}, , descarga ${descargando}`)
@@ -130,10 +115,6 @@ async function downloadTileImage(x, y, zoom) {
 
 function getTileFileDir(x, zoom) {
     return `${filesDir}map\\${zoom}\\${x}\\`
-}
-
-function getTileFileDir2(x, zoom) {
-    return `/map/${zoom}/${x}/`
 }
 
 module.exports = { doWork };
