49d16c3bcf0ca4832101019e419c6ffe574fa0d7
Yury Rivera <yrivera@applaudostudios.com>
2021-08-20 11:02:41 -0600

    usndo let y no var

--- copyMap.js
+++ copyMap.js
@@ -14,8 +14,8 @@ let total = 0
 let descargando = 0;
 let existencia = 0;
 let listPoints = [];
-const mLimit = 12
-const nLimit = 18
+const mLimit = 18;
+const nLimit = 15;
 let progress = 0
 let fallas = 0
 let punto;
@@ -30,58 +30,54 @@ class Point {
 
 async function doWork() {
   console.log("punto", punto);
-  defineFullMap()
-  console.log("to preocess");
+  defineFullMap();
   console.log("poinst", listPoints);
   console.log("total", total);
   await processMap();
 }
 
 async function copyMaps() {
-  let list = new Array
+  let list = [];
   list.push(new DepartmentMap(-89.279274, 13.676027, "santaTecla"));
-  list.push(new DepartmentMap(-89.203484, 13.699763, "sanSalvador"));
-  list.push(new DepartmentMap(-89.125745, 13.705742, "soyapango"));
-  list.push(new DepartmentMap(-89.178365, 13.801453, "apopa"));
-  list.push(new DepartmentMap(-89.125745, 13.705742, "soyapango"));
-  list.push(new DepartmentMap(-89.376742, 13.728885, "lourdes"));
-  list.forEach((e) => {
+  // list.push(new DepartmentMap(-89.203484, 13.699763, "sanSalvador"));
+  // list.push(new DepartmentMap(-89.125745, 13.705742, "soyapango"));
+  // list.push(new DepartmentMap(-89.178365, 13.801453, "apopa"));
+  // list.push(new DepartmentMap(-89.125745, 13.705742, "soyapango"));
+  // list.push(new DepartmentMap(-89.376742, 13.728885, "lourdes"));
+  for (const e of list) {
     punto = e;
-    doWork();
-  })
-
+    await doWork();
+  }
 }
 
 async function processMap() {
-  var flagList = 0
-  for (k = nLimit; k <= mLimit; k++) {
-    for (i = listPoints[flagList].x; i <= listPoints[flagList + 1].x; i++) {
-      var divide = listPoints[flagList + 1].y - listPoints[flagList].y
-      divide /= 10
-      divide = Math.trunc(divide)
+  let flagList = 0;
+  for (let k = nLimit; k <= mLimit; k++) {
+    for (let i = listPoints[flagList].x; i <= listPoints[flagList + 1].x; i++) {
+      let divide = listPoints[flagList + 1].y - listPoints[flagList].y;
+      divide /= 10;
+      divide = Math.trunc(divide);
       if (divide === 0) {
         divide = 1
       }
-      var initialvalue = listPoints[flagList].y
+      let initialvalue = listPoints[flagList].y;
       while (initialvalue < listPoints[flagList + 1].y) {
         if (initialvalue + divide > listPoints[flagList + 1].y) {
           await downloadPart(i, k, initialvalue, listPoints[flagList + 1].y);
         } else {
           await downloadPart(i, k, initialvalue, initialvalue + divide);
         }
-        //progress ++
         initialvalue += divide
       }
-
     }
-    flagList += 2
+    flagList += 2;
   }
 }
 
 async function downloadPart(i, k, a, b) {
-  var flag = 0;
+  let flag = 0;
   const totalint = b - a
-  for (j = a; j <= b; j++) {
+  for (let j = a; j <= b; j++) {
     let exceptione = await downloadTileImage(i, j, k);
     if (exceptione != null) {
       fallas++;
@@ -97,7 +93,7 @@ function defineFullMap() {
   var latTemp = null
   var longTemp = null
   var flag = 0
-  for (i = mLimit; i <= nLimit; i++) {
+  for (let i = nLimit; i <= mLimit; i++) {
     latTemp = punto.y + limite
     longTemp = punto.x - limite // -88.1397//-89.1397 //-90.1397//
     listPoints.push(new Point(getXm(longTemp, i), getYm(latTemp, i)))
@@ -108,7 +104,7 @@ function defineFullMap() {
       (listPoints[flag].x - listPoints[flag + 1].x) * (listPoints[flag].y - listPoints[flag + 1].y)
     flag += 2
   }
-  console.log("mapa definido")
+  console.log("mapa definido");
 }
 
 function getXm(longitude, i) {
@@ -140,11 +136,11 @@ async function downloadTileImage(x, y, zoom) {
 }
 
 function getTileFileDir(x, zoom) {
-  return `${filesDir}/map/${zoom}/${x}/`
+  return `${filesDir}map/${zoom}/${x}/`
 }
 
 function getTileFileNewDir(x, zoom) {
-  return `zonas/${punto.departamento}/map/${zoom}/${x}/`
+  return `${filesDir}zonas/${punto.departamento}/map/${zoom}/${x}/`
 }
 
 module.exports = {doWork, copyMaps};
